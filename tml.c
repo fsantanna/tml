0952249c155fb1c33c1ce75c3267b99577816975
Francisco Sant'Anna <francisco.santanna@gmail.com>
2022-07-01 15:52:01 -0300

    - trv

--- tml.c
+++ tml.c
@@ -16,54 +16,85 @@ void tml_loop (int fps, void(*cb_sim)(tml_evt), int(*cb_evt)(tml_evt*), int(*cb_
         uint32_t nxt;
         int      mpf;
         int      tick;
-    } T = { SDL_GetTicks()+mpf, mpf, 0 };
+    } S = { SDL_GetTicks()+mpf, mpf, 0 };
 
     struct {
         int tot;
         int nxt;
-        tml_tick_evt queue[EVT_MAX];
-    } E = { 0, 0, {} };
+        tml_tick_evt buf[EVT_MAX];
+    } Q = { 0, 0, {} };
 
     cb_sim((tml_evt) { TML_EVT_FIRST });
 
+_RET_EVT_: {
+
     while (1) {
-        if (E.nxt < E.tot) {
-            cb_sim(E.queue[E.nxt++].evt);
+        if (Q.nxt < Q.tot) {
+            cb_sim(Q.buf[Q.nxt++].evt);
         } else {
             uint32_t now = SDL_GetTicks();
-            if (now < T.nxt) {
-                SDL_WaitEventTimeout(NULL, T.nxt-now);
+            if (now < S.nxt) {
+                SDL_WaitEventTimeout(NULL, S.nxt-now);
                 now = SDL_GetTicks();
             }
-            if (now >= T.nxt) {
-                T.tick++;
-                T.nxt += T.mpf;
-                cb_sim((tml_evt) { TML_EVT_TICK, {.tick=T.tick} });
+            if (now >= S.nxt) {
+                S.tick++;
+                S.nxt += S.mpf;
+                cb_sim((tml_evt) { TML_EVT_TICK, {.tick=S.tick} });
             }
 
             tml_evt evt;
             switch (cb_evt(&evt)) {
                 case TML_RET_EVT:
-                    assert(E.tot < EVT_MAX);
-                    E.queue[E.tot++] = (tml_tick_evt) { T.tick, evt };
+                    assert(Q.tot < EVT_MAX);
+                    Q.buf[Q.tot++] = (tml_tick_evt) { S.tick, evt };
                     break;
-                case TML_RET_TRV: {
-                    uint32_t now = SDL_GetTicks();
-                    cb_sim((tml_evt) { TML_EVT_FIRST });
-                    int tick = 0;
-                    for (int i=0; i<E.tot; i++) {
-                        tml_tick_evt cur = E.queue[i];
-                        while (cur.tick > tick) {
-                            tick++;
-                            SDL_Delay(T.mpf);
-                            cb_sim((tml_evt) { TML_EVT_TICK, {.tick=T.tick} });
-                        }
-                        cb_sim(E.queue[i].evt);
-                    }
-                    T.nxt += (SDL_GetTicks() - now);
-                    break;
-                }
+                case TML_RET_TRV:
+                    goto _RET_TRV_;
             }
         }
     }
+}
+
+_RET_TRV_: {
+
+    uint32_t prv = SDL_GetTicks();
+    uint32_t nxt = SDL_GetTicks();
+    while (1) {
+        uint32_t now = SDL_GetTicks();
+        if (now < nxt) {
+            SDL_WaitEventTimeout(NULL, nxt-now);
+            now = SDL_GetTicks();
+        }
+        if (now >= nxt) {
+            nxt += S.mpf;
+        }
+
+        tml_trv trv;
+        switch (cb_trv(&trv)) {
+            case TML_RET_EVT:
+                S.nxt += (SDL_GetTicks() - prv);
+                goto _RET_EVT_;
+                break;
+            case TML_RET_TRV:
+                goto _RET_TRV_;
+        }
+    }
+}
+
+#if 0
+                    cb_sim((tml_evt) { TML_EVT_FIRST });
+                    int tick = 0;
+                    for (int i=0; i<Q.tot; i++) {
+                        tml_tick_evt cur = Q.buf[i];
+                        while (cur.tick > tick) {
+                            tick++;
+                            SDL_Delay(S.mpf);
+                            cb_sim((tml_evt) { TML_EVT_TICK, {.tick=S.tick} });
+                        }
+                        cb_sim(Q.buf[i].evt);
+                    }
+                    break;
+                }
+#endif
 }
