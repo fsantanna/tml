36b88fd34dbc4b2527e15eee11c977f418a0b7f3
Francisco Sant'Anna <francisco.santanna@gmail.com>
2022-07-06 20:27:40 -0300

    - bar click

--- trv.c
+++ trv.c
@@ -1,22 +1,22 @@
 #include <SDL2/SDL_image.h>
 
-static SDL_Texture* imgs[7];
-
+static SDL_Texture* IMGS[7];
 static int MAX=-1, CUR=-1;
+static SDL_Rect BAR;
 
 void trv_init (void) {
-	imgs[0] = IMG_LoadTexture(REN, "imgs/r0.png");
-	imgs[1] = IMG_LoadTexture(REN, "imgs/r1.png");
-	imgs[2] = IMG_LoadTexture(REN, "imgs/r2.png");
-	imgs[3] = IMG_LoadTexture(REN, "imgs/r3.png");
-	imgs[4] = IMG_LoadTexture(REN, "imgs/r4.png");
-	imgs[5] = IMG_LoadTexture(REN, "imgs/r5.png");
-	imgs[6] = IMG_LoadTexture(REN, "imgs/r6.png");
+	IMGS[0] = IMG_LoadTexture(REN, "imgs/r0.png");
+	IMGS[1] = IMG_LoadTexture(REN, "imgs/r1.png");
+	IMGS[2] = IMG_LoadTexture(REN, "imgs/r2.png");
+	IMGS[3] = IMG_LoadTexture(REN, "imgs/r3.png");
+	IMGS[4] = IMG_LoadTexture(REN, "imgs/r4.png");
+	IMGS[5] = IMG_LoadTexture(REN, "imgs/r5.png");
+	IMGS[6] = IMG_LoadTexture(REN, "imgs/r6.png");
 }
 
 void trv_quit (void) {
     for (int i=0; i<7; i++) {
-        SDL_DestroyTexture(imgs[i]);
+        SDL_DestroyTexture(IMGS[i]);
     }
 }
 
@@ -29,18 +29,18 @@ void trv_eff (void) {
         int y = trv_rs[0].y - 15;
         int x = trv_rs[5].x;
         int w = trv_rs[6].x + trv_rs[6].w - trv_rs[5].x;
-        SDL_Rect r = { x, y, w, 5 };
+        BAR = (SDL_Rect) { x, y, w, 5 };
         SDL_SetRenderDrawColor(REN, 0x00,0x00,0x00,0xFF);
-        SDL_RenderFillRect(REN, &r);
+        SDL_RenderFillRect(REN, &BAR);
 
         int T = (MAX>0) ? CUR*1000/MAX * w / 1000 : w;
-        SDL_Rect p = { x+T-1, y, 3, 5 };
+        SDL_Rect r = { x+T-1, y, 3, 5 };
         SDL_SetRenderDrawColor(REN, 0xFF,0xFF,0xFF,0xFF);
-        SDL_RenderFillRect(REN, &p);
+        SDL_RenderFillRect(REN, &r);
     }
 
     for (int i=0; i<7; i++) {
-        SDL_RenderCopy(REN, imgs[i], NULL, &trv_rs[i]);
+        SDL_RenderCopy(REN, IMGS[i], NULL, &trv_rs[i]);
     }
 }
 
@@ -74,7 +74,12 @@ int trv_cb (SDL_Event* sdl, int max, int cur, int* ret) {
             case SDL_MOUSEBUTTONDOWN: {
                 SDL_Point pt = { sdl->button.x, sdl->button.y };
                 for (int i=0; i<7; i++) {
-                    if (SDL_PointInRect(&pt, &trv_rs[i])) {
+                    if (SDL_PointInRect(&pt, &BAR)) {
+                        going = 0;
+                        *ret = (pt.x - BAR.x) * 1000 / BAR.w * max / 1000;
+                        CUR = *ret;
+                        return TML_RET_TRV;
+                    } else if (SDL_PointInRect(&pt, &trv_rs[i])) {
                         switch (i) {
                             case 0:
                                 if (going == 0) {
